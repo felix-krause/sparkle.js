6b555683c4cf184e9c970b65d26dbbbb86aa4430
MacBook von Felix <felix.s.krause@googlemail.com>
2019-10-12 14:54:59 +0200

    moving examples to jsfiddle

--- demo/sparkle-custom-properties/demo.js
+++ demo/sparkle-custom-properties/demo.js
@@ -1,10 +1,12 @@
-document.getElementById("sparkle").sparkle({
-    position: "afterbegin",
-    count: 25,
-    minAge: 500,
-    maxAge: 2000,
-    size: {
-        width: "15px",
-        height: "15px"
-    }
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("sparkle").sparkle({
+        position: "afterbegin",
+        count: 25,
+        minAge: 500,
+        maxAge: 2000,
+        size: {
+            width: "15px",
+            height: "15px"
+        }
+    });
 });
